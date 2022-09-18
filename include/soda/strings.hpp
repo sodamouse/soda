e0c1c43f088370b5ea3cae3f0d89b8808a0bfe52
sodamouse <sodamouse@proton.me>
2022-09-18 08:37:13 +0300

    Update include

--- include/soda/strings.hpp
+++ include/soda/strings.hpp
@@ -5,6 +5,8 @@
 
 namespace soda {
 
+std::string quotify(const std::string& str);
+std::string read_all(const std::string& filePath);
 std::vector<std::string> split_string(const std::string& str, char delim);
 
 }
