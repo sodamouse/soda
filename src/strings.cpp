fd89b75bd526cc0a981a8b2e19ad60333328b52f
sodamouse <sodamouse@proton.me>
2022-09-18 07:34:34 +0300

    String split implementation

--- src/strings.cpp
+++ src/strings.cpp
@@ -12,11 +12,11 @@ std::vector<std::string> split_string(const std::string& str, char delim)
   c = str.find(delim);
   while (c != std::string::npos)
   {
-    c = str.find(delim);
+    c = str.find(delim, p);
     auto sub = str.substr(p, c - p);
     result.push_back(sub);
 
-    p = c;
+    p = c + 1;
   }
 
   return result;
