6296306acceb04de0a06c4ecc42a6180048094c5
sodamouse <sodamouse@proton.me>
2022-10-01 20:29:04 +0300

    Add environment

---
+++ src/environment.cpp
@@ -0,0 +1,16 @@
+#include "environment.hpp"
+
+namespace soda {
+
+std::unordered_map<int, std::string> map_args(int argc, char* argv[])
+{
+    std::unordered_map<int, std::string> result;
+    for (int i = 0; i < argc; ++i)
+    {
+        result.insert(std::make_pair(i, argv[i]));
+    }
+
+    return result;
+}
+
+}
