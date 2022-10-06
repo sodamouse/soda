35c906e2966263274bb34cda2536ae804b45f599
sodamouse <sodamouse@proton.me>
2022-10-06 17:37:04 +0300

    Deprecate soda::quotify

--- src/strings.cpp
+++ src/strings.cpp
@@ -5,15 +5,6 @@
 
 namespace soda {
 
-std::string quotify(const std::string& str)
-{
-    std::string result = "\"";
-    result += str;
-    result += "\"";
-
-    return result;
-}
-
 std::string read_all(const std::string& filePath)
 {
     std::fstream file(filePath, std::ios::in);
--- src/strings.hpp
+++ src/strings.hpp
@@ -5,7 +5,6 @@
 
 namespace soda {
 
-std::string quotify(const std::string& str);
 std::string read_all(const std::string& filePath);
 std::vector<std::string> split_string(const std::string& str, char delim);
 
