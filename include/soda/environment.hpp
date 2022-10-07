1249930f502bf303f7f10b9dd4e11fe42a926286
sodamouse <sodamouse@proton.me>
2022-10-07 10:00:50 +0300

    Update include

--- include/soda/environment.hpp
+++ include/soda/environment.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <utility>
 
 namespace soda {
 
-std::unordered_map<int, std::string> map_args(int argc, char* argv[]);
+std::map<int, std::string> map_args(int argc, char* argv[]);
 
 }
